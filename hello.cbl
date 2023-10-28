73c7289f1f51e5ea0fcc38f3be1876f096452f46
enkhjargal <enkhjargal.dev@gmail.com>
2023-10-28 23:41:02 +0900

    hello world

---
+++ hello.cbl
@@ -0,0 +1,7 @@
+       IDENTIFICATION DIVISION.
+           PROGRAM-ID. hello_world.
+           AUTHOR. "Enkhjargal Lkhagvaa".
+
+       PROCEDURE DIVISION.
+           DISPLAY "Hello COBOL".
+           STOP RUN.
